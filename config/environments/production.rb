6ab31717a9df6d9ae804ac05faaf34a7a707bbda
brianqeep <brites009@gmail.com>
2017-04-18 13:24:04 +0300

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  Sunspot.config.solr.url = 'http://localhost:8983/solr'
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
